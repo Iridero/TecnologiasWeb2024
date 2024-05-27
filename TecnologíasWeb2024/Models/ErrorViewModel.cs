73098995939dbacb6584a17730a14435a70e2009
iridero <jcgarza_mx@hotmail.com>
2024-05-27 12:08:47 -0600

    Agregar archivos de proyecto.

---
+++ TecnologíasWeb2024/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TecnologíasWeb2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
